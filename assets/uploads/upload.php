569ebce89bd7241edca1ef475084a0a39974b4b6
wilfort <wilfort.stephane@hotmail.fr>
2018-08-03 17:58:53 +0200

    batra

---
+++ assets/uploads/upload.php
@@ -0,0 +1,30 @@
+<?php
+
+require ('./vendor/verot/class.upload.php/src/class.upload.php');
+
+function uploader (){
+    $today= date('d/m/Y H:i:s');
+    $handle = new upload($_FILES['upload']);
+    if ($handle->uploaded){
+        
+        if ($handle->file_src_name_ext!='bmp' && $handle->file_is_image=='true'){
+            $handle->process('./assets/img/uploads/');
+           
+            if ($handle->processed){
+                // echo 'image uploaded';
+                file_put_contents('./assets/uploads/logUploads.txt',$today.' upload de '.$handle->file_src_name.' vers '.$handle->file_dst_name."\n",FILE_APPEND);
+            }
+            else{
+                echo 'error'.$handle->error;
+            }
+        }
+        else{
+            echo 'type de fichier non supporté';
+            file_put_contents('./assets/uploads/logUploads.txt',$today.' upload de '.$handle->file_src_name.':fichier non-supporté '."\n",FILE_APPEND);
+        }
+    }
+}
+uploader();
+
+//phpinfo();
+?>
