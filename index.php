ea6811a3b7008920f6fe72323a589321df2904b7
wilfort <wilfort.stephane@hotmail.fr>
2018-08-03 15:25:55 +0200

    batra

---
+++ index.php
@@ -0,0 +1,268 @@
+<?php 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php include('./assets/vue/link.php') ?>
+</head>
+<body>
+<form action="" method="post">
+    <!-- dénomination -->
+        <label for="">Dénomination</label>
+        <input type="text" name="" id="">
+    <br>
+    <!-- provenance -->
+        <label for="">Provenance</label>
+        <input type="text" name="" id="">
+    <br>
+    <!-- liste ingrdiant -->
+        <label for="">Liste ingrdiant</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    <br>
+    <!-- alégéne -->
+        <b>liste des allergènes</b><br>
+            <ol>
+                <li>
+                    <label for="">Gluten :</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Crustacés</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Oeufs</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Poissons</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Arachides</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Soja</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li> 
+                    <label for="">Lait</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Fruits à coque</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Céleri</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Moutarde</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Graines de sésame</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label><br>
+                </li>
+                <li>
+                    <label for="">Anhydride sulfureux et sulfites</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Lupin</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+                <li>
+                    <label for="">Mollusques</label>
+                        <input type="radio" name="" id=""><label for="">ne contenient pas</label>
+                        <input type="radio" name="" id=""><label for="">contenient</label>
+                        <input type="radio" name="" id=""><label for="">trase</label>
+                        <input type="radio" name="" id=""><label for="">autre</label>
+                </li>
+            </ol>
+    <br>
+    <!-- raison social -->
+        <label for="">Raison social</label>
+        <input type="radio" name="" id="">SA
+        <input type="radio" name="" id="">SPRL
+        <input type="radio" name="" id="">ASBL
+        <input type="radio" name="" id="">Autre
+    <br>
+    <!-- nom entreprise -->
+        <label for="">Nom entreprise</label>
+        <input type="text" name="" id="">
+    <br>
+    <!-- Conservation -->
+        <label for="">Conservation</label>
+        <input type="date" name="" id="">
+    <br>
+    <!-- adresse entreprise -->
+        <label for="">Adresse entreprise</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    <br>
+    <!-- déclaration nutritionnelle -->
+        <label for="">Déclaration nutritionnelle</label>
+        <ul>
+            <li>Énergie <input type="number" name="" id=""> </li>
+            <li>Graisses totales <input type="number" name="" id=""></li>
+            <li>Acides gras saturés <input type="number" name="" id=""></li>
+            <li>Glucides <input type="number" name="" id=""></li>
+            <li>Sucres <input type="number" name="" id=""></li>
+            <li>Protéines <input type="number" name="" id=""></li>
+            <li>Sel <input type="number" name="" id=""></li>
+            <li>Autre <input type="number" name="" id=""></li>
+        </ul>
+
+        
+    <br>
+    <!-- contact -->
+        <label for="">Contact</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+    <br>
+    <!-- pays origine -->
+        <label for="">Pays origine</label>
+        <input type="text" name="" id="">
+    <br>
+    <!-- Teneur en alcool -->
+        <label for="">Teneur en alcool</label>
+    <br>
+</form>
+     <!-- <div class="row"> 
+         <div class="col-md-12 col-lg-6">
+             <div class="row">
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+                
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <div class="col-md-12 col-lg-6">
+             <div class="row">
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+
+                <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+                
+                 <div class="row">
+                     <div class="col-md-12 col-lg-9">
+                     </div>
+                     <div class="col-md-12 col-lg-3">
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div> -->
+</body>
+</html>
