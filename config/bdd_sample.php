4566faf0e51dcd0cea58fc072180da574e14421c
Steve Van Essche <steve.vanessche@gmail.com>
2018-08-06 11:52:28 +0200

    Ajout d'un fichier exemple pour la connexion à la BDD

---
+++ config/bdd_sample.php
@@ -0,0 +1,6 @@
+<?php
+// Renommé ce fichier en "bdd.php" et modifier les infos ci-dessous pour que cela corresponde à votre BDD
+$dbHost = "localhost";
+$dbName = "batra";
+$dbUser = "root";
+$dbPassword = "password";
